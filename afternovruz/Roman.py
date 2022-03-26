faeecd1d2d84d0ebfdc31b45a340c54cbf35d4ae
YasharHTML <85059801+YasharHTML@users.noreply.github.com>
2022-03-27 00:10:36 +0400

    Added new roman and edited D

---
+++ afternovruz/Roman.py
@@ -0,0 +1,23 @@
+li = ["I", "V", "X", "L", "C", "D", "M"]
+
+def roman_to_int(s):
+        rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        int_val = 0
+        for i in range(len(s)):
+            if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
+                int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
+            else:
+                int_val += rom_val[s[i]]
+        return int_val
+# if a string is only composed of roman numerals
+def is_roman(s):
+    for i in range(len(s)):
+        if s[i] not in li:
+            return False
+    return True
+
+n = input("Enter ur sentence: ").split()
+for i in range(len(n)):
+    if is_roman(n[i]):
+        n[i] = roman_to_int(n[i])
+print(*n)
